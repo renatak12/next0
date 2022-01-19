fd9b152669b2916570b5e7d63a862453bc04513f
Renata Araújo <renatakarla02@gmail.com>
2022-01-18 23:39:39 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,10 @@ import {Titulo} from "../components/main.js"
 export default function Home(){
   return (
     <div>
-
+      <Titulo titulo="Tarefa de PWeb Receita-Next-01"/>
       <h2>
         Viva Santana!
       </h2>
-      <Titulo titulo="Tarefa de PWeb Receita-Next-01"/>
-      
     </div>
   )
 }
