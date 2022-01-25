2c0c102899f9d9fca4342a341f5974e6a234de24
Renata Araújo <renatakarla02@gmail.com>
2022-01-24 21:25:14 -0300

    Update novaPagina.js

--- pages/receita2/novaPagina.js
+++ pages/receita2/novaPagina.js
@@ -1,9 +1,12 @@
+import Link from "next/link"
+
 export default function Principal(){
 
    return (
       <div>
          <h1>Nova Página</h1>
          <MariaPrea/>
+         <Link href='/'><a>Voltar</a></Link>
       </div>
    )
 }
