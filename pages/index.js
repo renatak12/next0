3b9145780b113b445881db515011d97be37147d7
Renata Araújo <renatakarla02@gmail.com>
2022-01-24 22:18:49 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,37 @@ import Link from 'next/link'
 export default function Home(){
   return (
     <div>
+    
+        <style jsx>{`
+                
+                nav {
+                    margin-top: 20px;
+                    display: flex;
+                    gap: 10px;
+                    justify-content: center;
+                }
+                
+                a:hover {
+                    color: #0489B1;
+                }
+            `}
+        </style>
+
+        <nav>
         <Link href="/receita2/novaPagina">
           <a> Nova Pagina </a>
         </Link>
         <Link href="/receita2/about">
           <a> Sobre nós </a>
         </Link>
-      <Titulo titulo="Tarefa de PWeb Receita-Next-02"/>
-      <h2>
-        Viva Santana!
-      </h2>
-      <Titulo2 />
+        </nav>
+
+        <Titulo titulo="Tarefa de PWeb Receita-Next-02"/>
+        <h2>
+          Viva Santana!
+        </h2>
+        <Titulo2 />
     </div>
   )
 }
+
