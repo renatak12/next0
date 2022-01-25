87431115e6e361fb1d3513d86b255cd066a6379b
Renata Araújo <renatakarla02@gmail.com>
2022-01-24 23:00:11 -0300

    atualizando a Receita-next-01

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import {Titulo} from "../components/Titulo.js"
+import {Titulo2} from "../components/Titulo2.js"
+
+export default function Home(){
+  return (
+    <div>
+        <Titulo titulo="Tarefa de PWeb Receita-Next-01"/>
+        <h2>
+          Viva Santana!
+        </h2>
+        <Titulo2 />
+    </div>
+  )
+}
