7937c471fd0dba4ad061b8533a02949981702540
Renata Araújo <renatakarla02@gmail.com>
2022-01-25 23:52:57 -0300

    Update [receita3].js

--- pages/receita3/[receita3].js
+++ pages/receita3/[receita3].js
@@ -1,1 +1,62 @@
+import Link from 'next/link'
 
+export default function Movies({data}){
+  return (
+    <div>
+      <style jsx>{`
+        a{
+          color: blue;
+          text-decoration: none;
+          display: flex;
+          justify-content: center;
+          padding-top: 3%;
+        }
+        img {
+          width: 150px;
+        }
+        h1{
+          display: flex;
+          justify-content: center;
+        }
+        label {
+          margin-left: 60%;
+        }
+        #content {
+          display: flex;
+          justify-content: center;
+        }
+        #cabecalho {
+          padding-top: 0%;
+          padding-bottom: 1%;
+        }
+      `}
+      </style>
+      <h1>Movies</h1>
+
+      <div id="map">
+        {data.Search.map( (m) => 
+          <div id="content">
+            Titulo: {m.Title} -- Ano: {m.Year} -- 
+            <img src={m.Poster}></img>
+          </div>  
+        )}               
+      </div>
+
+      <Link href="/">
+      <a>Voltar</a>
+      </Link>
+    </div>
+  )
+}
+
+export async function getServerSideProps(context){
+  //const res = await fetch(`http://www.omdbapi.com/?apikey=86b9eaa3&s=bagdad`)
+  const res = await fetch(`http://http://www.omdbapi.com/?apikey=86b9eaa3&s=${context.params.receita3}`)
+  const data = await res.json()
+
+  return {
+    props: {
+      data
+    }
+  }
+}
