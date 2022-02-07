61969d93a5c92d43ceff02df92547dd591a87c25
Renata Araújo <renatakarla02@gmail.com>
2022-02-07 17:35:47 -0300

    Update [id].js

--- pages/receita4/exercicio1/detailsID/[id].js
+++ pages/receita4/exercicio1/detailsID/[id].js
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router'
 export default function MovieID(){
   const router = useRouter()
   const {id} = router.query
-  const {data, error} = useSWR(`http://www.omdbapi.com/?apikey=86b9eaa3&s=bagdad`, fetcher)
+  const {data, error} = useSWR(`http://www.omdbapi.com/?apikey=86b9eaa3&s=2020`, fetcher)
   console.log(data)    
   if (error) return <div>falha na requisição...</div>
   if (!data) return <div>carregando...</div>
