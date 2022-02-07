330456368460f482be6952421f4c50d8eb2b9607
Renata Araújo <renatakarla02@gmail.com>
2022-02-07 16:15:17 -0300

    Update and rename pages/receita4/exercicio2/[movies2].js to pages/receita4/exercicio3/[movies2].js

---
+++ pages/receita4/exercicio3/[movies2].js
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+
+export default function Movies2({data}){  
+    return (
+        <ul>
+            {data.Search.map( (m, i) => 
+               <li key={i}>
+                  <Link href={`//receita4/exercicio3/detailsID3/${m.imdbID}`}><a>{m.Title}</a></Link>
+               </li>
+            )}
+        </ul>
+    )    
+}
+
+export async function getServerSideProps(context){
+  const res = await fetch(`/http://www.omdbapi.com/?apikey=86b9eaa3&s=${context.params.movies2}`)
+  const data = await res.json()
+  return {
+    props: {
+      data
+    }
+  }
+}
