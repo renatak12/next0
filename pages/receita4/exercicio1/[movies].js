f84336d8e74f5cfeaaf081f4319d43a723e5fa33
Renata Araújo <renatakarla02@gmail.com>
2022-02-07 16:11:24 -0300

    Update [movies].js

--- pages/receita4/exercicio1/[movies].js
+++ pages/receita4/exercicio1/[movies].js
@@ -1,1 +1,28 @@
+import useSWR from 'swr'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
+export default function Movies(){
+    const router = useRouter()
+    const movie = router.query.movies
+    const {data, error} = useSWR(`http://www.omdbapi.com/?apikey=86b9eaa3&s=${movie}`, fetcher)    
+
+    if (error) return <div>falha na requisição...</div>
+    if (!data) return <div>carregando...</div>
+
+    return (
+        <ul>
+            { data.Search.map( (m, i) => 
+               <li key={i}>
+                  <Link href={`//receita4/exercicio1/detailsID/${m.imdbID}`}><a>{m.Title}</a></Link>
+               </li>
+            )}
+        </ul>
+    )    
+}
+
+async function fetcher(url) {
+    const res = await fetch(url);
+    const json = await res.json();
+    return json;
+}
