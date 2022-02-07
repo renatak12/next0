f11b0bdce14ea6358d82eea7b6a657631ba23040
Renata Araújo <renatakarla02@gmail.com>
2022-02-07 16:07:16 -0300

    Update [id].js

--- pages/receita4/exercicio1/detailsID/[id].js
+++ pages/receita4/exercicio1/detailsID/[id].js
@@ -1,1 +1,24 @@
+import useSWR from 'swr'
+import { useRouter } from 'next/router'
 
+export default function MovieID(){
+  const router = useRouter()
+  const {id} = router.query
+  const {data, error} = useSWR(`http://www.omdbapi.com/?apikey=86b9eaa3&s=${id}`, fetcher)
+  console.log(data)    
+  if (error) return <div>falha na requisição...</div>
+  if (!data) return <div>carregando...</div>
+
+  return (
+    <div id="content">
+      Titulo: {data.Title} -- Ano: {data.Year} -- 
+      <img src={data.Poster}></img>
+    </div>  
+  )    
+}
+
+async function fetcher(url) {
+  const res = await fetch(url);
+  const json = await res.json();
+  return json;
+}
