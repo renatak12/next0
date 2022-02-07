694895a82849f6809c53dfbb3d3e9f74907db2ed
Renata Araújo <renatakarla02@gmail.com>
2022-02-07 20:27:24 -0300

    Update [movieID].js

--- pages/receita5/exercicio2/movDet/[movieID].js
+++ pages/receita5/exercicio2/movDet/[movieID].js
@@ -1,4 +1,4 @@
-import MovDet from "../../receita4/exercicio1/detailsID/[movieID]";
+import MovDet from "../../../receita4/exercicio1/detailsID/[movieID]";
 
 export default function Exercicio2 (){
     return <MovDet />
