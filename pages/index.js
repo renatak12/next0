7573f3acda8eae6866c58834646eb9a57b20ea65
Renata Araújo <renatakarla02@gmail.com>
2022-02-07 16:00:34 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,7 @@ export default function Home(){
         <Link href="/receita2/novaPagina"><a> Nova Pagina </a></Link>
         <Link href="/receita3/bagdad"><a> Filmes </a></Link>
         <Link href="/receita2/about"><a> Sobre nós </a></Link>
+        <Link href="/receita4/exercicio1/bagdad"><a> Receita4 </a></Link>
         </nav>
         <Titulo titulo="Tarefa de PWeb Receitas-Next"/>
         <h2>
