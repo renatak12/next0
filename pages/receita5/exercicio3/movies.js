c87eb5bd666031b56543b105c3baadbb2317795d
Renata Araújo <renatakarla02@gmail.com>
2022-02-07 21:04:14 -0300

    Create movies.js

---
+++ pages/receita5/exercicio3/movies.js
@@ -0,0 +1,74 @@
+import {useState} from 'react'
+
+export default function Movies3(){
+
+    const [url, setUrl] = useState('')
+    const [data, setData] = useState('')
+
+    const onClickHandler = async (e) => {
+        const link = e.target.id
+
+        e.preventDefault()
+        if (url === ''){
+            setUrl(link)
+            const result = await theFetcher(link)
+            console.log(result)
+            setData(result)
+            return Movies3
+        }else {
+            setUrl('')
+            setData('')
+            return Movies3
+        }
+
+        
+
+    }
+
+    return (
+
+        <div>
+            <TheLink url={url} link={'https://www.omdbapi.com/?apikey=a85a78d&s=2010'} handler={onClickHandler} />
+            <TheMovies data={data}/>
+        </div>
+
+    )
+
+}
+
+async function theFetcher(url) {
+
+    const res = await fetch(url);
+    const json = await res.json();
+    return json;
+
+}
+
+export function TheMovies({data}){
+
+    if(data == '') return <div></div>
+
+    return (
+
+        <div>
+            { data.Search.map( (m, i) => <div key={i}>{m.Title} --- {m.Year}</div>  ) }            
+
+        </div>
+
+    )
+
+}
+
+export function TheLink({url, handler, link}){    
+
+    return (
+
+        <div>
+
+            <a href="/receitas/receita5/exercicio3/movies.js" id={link} onClick={handler}> {url === '' ? 'Mostrar' : 'Ocultar'} </a>
+
+        </div>
+
+    )
+
+}
